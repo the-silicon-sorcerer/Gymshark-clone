9b1c5fde7f4040829d8e34753535f9138eb816a8
the-silicon-sorcerer <Kanahelebusiness@gmail.com>
2022-12-01 16:19:06 -0800

    hero and main secotion done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import heroImage from './assets/ezgif-5-774f11576e.jpg'
+import heroImage from './assets/hero.image.webp'
 import logo from './assets/gymshark-logo-3F1E7A14A4-seeklogo.com.png'
 import './App.css';
 
@@ -7,20 +7,20 @@ function App() {
     <div className='app'>
       <div className='headers'>
         <div className='utils-nav'>
-          <a className='util-link'>My Account</a>
-          <a className='util-link'>Blog</a>
-          <a className='util-link'>Email Sign up</a>
-          <a className='util-link'>Help</a>
-          <a className='util-link'>O United States ?</a>
+          <a className='util-link' href="#">My Account</a>
+          <a className='util-link' href="#">Blog</a>
+          <a className='util-link' href="#">Email Sign up</a>
+          <a className='util-link' href="#">Help</a>
+          <a className='util-link' href="#">O United States ?</a>
         </div>
         <div className='main-nav'>
           <div className='main-nav-link nav-logo-container'>
             <img src={logo}></img>
           </div>
           <div className='main-nav-link nav-categories-container'>
-            <div className='nav-categories'>Womans Sale</div>
-            <div className='nav-categories'>Mens Sale</div>
-            <div className='nav-categories'>Accessories Sale</div>
+            <div className='nav-categories'>Womans</div>
+            <div className='nav-categories'>Mens</div>
+            <div className='nav-categories'>Accessories</div>
           </div>
           <div className='main-nav-link nav-icons-container'>
             <div className='nav-icons'>1</div>
@@ -35,7 +35,32 @@ function App() {
         <p>Want e-mails you'll actually read? Sign up for exclusive deals, our latest drops, and more!</p>
       </div>
       <div className="main-body">
-        <img className='hero-image' src={heroImage} />
+        <div className='hero-container'>
+          <img className='hero-image' src={heroImage} />
+          <div className='hero-overlay'>
+            <div className='hero-title-container'>
+              <h1 className='hero-title'>GIFT GYMSHARK</h1>
+              <p className='hero-copy'>Take a look at this season's best sellers<br />to find the hottest gifts.</p>
+              <div className='hero-title-links-container'>
+                <div className="hero-link">gifts for her</div>
+                <div className="hero-link">gifts for him</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='card-container'>
+          <div className='collection-topic-container'>
+            <div className='collection-title'>
+              <h2>GYMSHARK GIFTS</h2>
+            </div>
+            <div className='collection-link'>
+              <a href='#'>view all</a>
+            </div>
+          </div>
+          <div className='cards-container'>
+
+          </div>
+        </div>
       </div>
     </div>
   );
